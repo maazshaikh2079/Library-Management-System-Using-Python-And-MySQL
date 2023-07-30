be90adfeb0bd323960b07bc5fb2a0d0f43af4575
maazshaikh2079 <124486797+maazshaikh2079@users.noreply.github.com>
2023-07-30 16:57:28 +0530

    Update LMSDB_Script.sql

--- LMSDB_Script.sql
+++ LMSDB_Script.sql
@@ -15,21 +15,22 @@ use lmsdb;
 -- desc library;
 -- desc borrowed;
 
--- insert into Library(Book_ID,Book_Title,Author) values(1111,"Python","G.V. Rossom");
--- insert into Library(Book_ID,Book_Title,Author) values(1112,"C","Dennis Ritchie");
--- insert into Library(Book_ID,Book_Title,Author) values(1113,"Java","James Gosling");
+-- INSERT INTO Library (Book_ID, Book_Title, Author) VALUES (1111, "The Universe Explained", "Alex Turner");
+-- INSERT INTO Library (Book_ID, Book_Title, Author) VALUES (1112, "Java Programming", "John Smith");
+-- INSERT INTO Library (Book_ID, Book_Title, Author) VALUES (1113, "C++ Primer", "Stanley Lippman");
+-- INSERT INTO Library (Book_ID, Book_Title, Author) VALUES (1114, "Data Science for Beginners", "Amanda Brown");
+-- INSERT INTO Library (Book_ID, Book_Title, Author) VALUES (1115, "Web Development 101", "Sarah Johnson");
+-- INSERT INTO Library (Book_ID, Book_Title, Author) VALUES (1116, "A.I. Fundamentals", "Michael Chen");
+-- INSERT INTO Library (Book_ID, Book_Title, Author) VALUES (1117, "Machine Learning Basics", "Emily Adams");
+-- INSERT INTO Library (Book_ID, Book_Title, Author) VALUES (1118, "Mathematics for Programmers", "David Lee");
+-- INSERT INTO Library (Book_ID, Book_Title, Author) VALUES (1119, "Chemistry Essentials", "Jennifer White");
+-- INSERT INTO Library (Book_ID, Book_Title, Author) VALUES (1120, "Physics Fundamentals", "Robert Johnson");
 
--- insert into Borrowed(Borrower,Phone_No,Book_ID,Book_Title,Author) values("Maaz",7208613298,1114,"HTML","Tim Berners-Lee");
--- insert into Borrowed(Borrower,Phone_No,Book_ID,Book_Title,Author) values("Wahaj",9321627175,1115,"CSS","HÃ¥kon Wium Lie");
+-- insert into Borrowed(Borrower,Phone_No,Book_ID,Book_Title,Author) values("Maaz",7208613298,1121,"JS: The Good Parts","Douglas Crockford");
+-- insert into Borrowed(Borrower,Phone_No,Book_ID,Book_Title,Author) values("Wahaj",9321627175,1122,"The Pragmatic Programmer","Andrew Hunt");
 
 select * from Borrowed;
 select * from LIBRARY;
 
-
-
-
-
-
-
-
-
+-- TRUNCATE TABLE Library;
+-- TRUNCATE TABLE Borrowed;
